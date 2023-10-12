217079cfe2681d3e666da47f15b33555159d35b0
Shayan <shayan.dameshghi@gmail.com>
2023-10-12 08:49:03 +0330

    LaTeX rendered in the new fragment.

--- app/src/main/java/ajman/shd/app1/fragments/JoistDesignFragment.kt
+++ app/src/main/java/ajman/shd/app1/fragments/JoistDesignFragment.kt
@@ -9,7 +9,9 @@ import androidx.fragment.app.Fragment
 import ajman.shd.app1.entities.JoistDesign
 import ajman.shd.app1.databinding.FragmentJoistDesignBinding
 import ajman.shd.app1.models.structure.Occupancy
+import ajman.shd.app1.models.structure.m
 import android.os.Build
+import android.webkit.WebView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
@@ -20,6 +22,7 @@ class JoistDesignFragment : Fragment() {
 
     private var _binding: FragmentJoistDesignBinding? = null
     private val binding get() = _binding!!
+    private var joistDesign: JoistDesign? = null
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreateView(
@@ -27,22 +30,10 @@ class JoistDesignFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentJoistDesignBinding.inflate(inflater, container, false)
-        val joistDesign: JoistDesign? =
-            arguments?.getParcelable("joistDesign", JoistDesign::class.java)
+        joistDesign = arguments?.getParcelable("joistDesign", JoistDesign::class.java)
         setSpinnerOccupancyValues()
-        setFieldValues(joistDesign)
+        joistDesign?.let { convertDataToFrom(it) }
         return binding.root
-    }
-
-    private fun setFieldValues(joistDesign: JoistDesign?) {
-        val root: View = binding.root
-        val projectNameTextView: TextView = root.findViewById(R.id.editTextProjectName)
-        val joistLengthTextView: TextView = root.findViewById(R.id.editTextJoistLength)
-        val occupancyTextView: Spinner = root.findViewById(R.id.spinnerOccupancy)
-
-        projectNameTextView.text = joistDesign?.projectName
-        joistLengthTextView.text = joistDesign?.joistLength.toString()
-        occupancyTextView.setSelection(joistDesign?.occupancy?.ordinal ?: -1)
     }
 
     private fun setSpinnerOccupancyValues() {
@@ -57,11 +48,39 @@ class JoistDesignFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Button>(R.id.buttonAnalyze).setOnClickListener {
-            analyzeJoistDesign(it)
+            joistDesign?.let {
+                convertFormToData(it)
+                it.analyze()
+                convertDataToFrom(it)
+            }
         }
     }
 
-    private fun analyzeJoistDesign(view: View) {
-        val x = 0
+    private fun convertDataToFrom(joistDesign: JoistDesign) {
+        val root: View = binding.root
+        root.findViewById<TextView?>(R.id.editTextProjectName).text = joistDesign.projectName
+        root.findViewById<TextView?>(R.id.editTextJoistLength).text =
+            (joistDesign.joistLength / m).toString()
+        root.findViewById<Spinner?>(R.id.spinnerOccupancy)
+            .setSelection(joistDesign.occupancy.ordinal)
+        renderLaTeX(joistDesign.requirementApplication.latexLines)
+    }
+
+    private fun renderLaTeX(latexLines: MutableList<String>) {
+        val document = latexLines.joinToString("\\\\") { latexLine ->
+            "\\displaystyle $latexLine"
+        }
+        val html =
+            "<!DOCTYPE html><html lang=\"en\"><head><script type=\"text/x-mathjax-config\">MathJax.Hub.Config({displayAlign:\"left\"});</script><script type=\"text/javascript\" async src=\"file:///android_asset/mathjax/Mathjax.js?config=TeX-AMS_CHTML\"></script></head><body>$$\\begin{array}{l}${document}\\end{array}$$</body></html>"
+        val webView = binding.root.findViewById<WebView>(R.id.webViewResultTable)
+        webView?.settings?.javaScriptEnabled = true
+        webView?.loadDataWithBaseURL("file:///android_asset/", html, "text/html", "utf-8", null)
+    }
+
+    private fun convertFormToData(joistDesign: JoistDesign) {
+        joistDesign.projectName = binding.editTextProjectName.text.toString()
+        joistDesign.joistLength =
+            (binding.editTextJoistLength.text.toString().toDoubleOrNull() ?: 0.0) * m
+        joistDesign.occupancy = binding.spinnerOccupancy.selectedItem as Occupancy
     }
 }
