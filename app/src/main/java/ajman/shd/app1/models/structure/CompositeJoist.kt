30e03b4bd51916e051f3cf6967b6c4a0c6aa6646
Shayan <shayan.dameshghi@gmail.com>
2023-10-13 13:06:04 +0330

    b_w Bug

--- app/src/main/java/ajman/shd/app1/models/structure/CompositeJoist.kt
+++ app/src/main/java/ajman/shd/app1/models/structure/CompositeJoist.kt
@@ -4,7 +4,7 @@ class CompositeJoist(var L: Double, val areaLoading: AreaLoading, var hasConcret
     var d: Double = 30 * cm
     var h: Double = 5 * cm
     var b: Double = 70 * cm
-    var bw: Double = if (hasConcreteWeb) b - 67.0 * cm else 0.0
+    var bw: Double = if (hasConcreteWeb) b - 66.0 * cm else 0.0
     var lineLoading = LineLoading(areaLoading, this)
     var steelJoist = SteelJoist(L, 3.6 * cm2)
     var concreteMaterial = Concrete(C20)
